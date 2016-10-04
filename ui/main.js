9d1d9f60fc6dd68950891fe01eb825d87a0a7498
anirudhsrinath1991 <anirudh.srinath1991@gmail.com>
2016-10-04 17:32:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,22 @@ span.innerHTML = counter.toString();
 request.open('GET', 'http://anirudhsrinath1991.imad.hasura-app.io/counter',true);
 request.send(null);
 }
+
+//Create a Submit Form
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick = function() {
+    
+}
+
+// Capture a list of names and render it as a list
+var names = ['name1','name2','name3','name4'];
+var list = ''';
+for (var i=0;i< names.length;i++){
+    list = '<li>' + names[i] + '</li>';
+}
+var ul = document.getElementById('nameList');
+ul.InnerHTML = list;
+
+
